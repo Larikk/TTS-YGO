4ec1a29bd52edd42249bcb22e03328ed294cc7fd
larikk <larikk.dev@gmail.com>
2020-08-27 00:32:44 +0200

    Doubled the size of box textures

--- non-tts-utilities/box-texture-creator/create-texture.py
+++ non-tts-utilities/box-texture-creator/create-texture.py
@@ -11,7 +11,7 @@ def pasteAlphaComposite(background, foreground):
     background.alpha_composite(foreground, offset)
 
 def createFront(logo, scale):
-    size = (144, 192)
+    size = (290, 386)
     logoSize = scaleTuple(size, scale) if scale != 1 else size
     front = Image.new("RGBA", size, "black")
     logo = logo.rotate(90, expand = True)
@@ -20,42 +20,42 @@ def createFront(logo, scale):
     return front
 
 def addTop(img):
-    top = Image.new("RGBA", (144, 96), "black")
-    img.paste(top, (108, 4))
+    top = Image.new("RGBA", (290, 194), "black")
+    img.paste(top, (215, 7))
 
 def addBottom(img):
-    bottom = Image.new("RGBA", (144, 96), "black")
-    img.paste(bottom, (364, 4))
+    bottom = Image.new("RGBA", (290, 194), "black")
+    img.paste(bottom, (727, 7))
 
 def addLeft(img, logo):
-    size = (96, 192)
+    size = (194, 386)
     logoSize = scaleTuple(size, 0.8)
     left = Image.new("RGBA", size, "black")
     logo = logo.rotate(-90, expand = True)
     logo.thumbnail(logoSize)
     pasteAlphaComposite(left, logo)
-    img.paste(left, (4, 108))
+    img.paste(left, (7, 215))
 
 def addRight(img, logo):
-    size = (96, 192)
+    size = (194, 386)
     logoSize = scaleTuple(size, 0.8)
     left = Image.new("RGBA", size, "black")
     logo = logo.rotate(90, expand = True)
     logo.thumbnail(logoSize)
     pasteAlphaComposite(left, logo)
-    img.paste(left, (260, 108))
+    img.paste(left, (519, 215))
 
 def addFront(img, front):
-    img.paste(front, (108, 108))
+    img.paste(front, (215, 215))
 
 def addBack(img):
-    back = Image.new("RGBA", (144, 192), "black")
-    img.paste(back, (364, 108))
+    back = Image.new("RGBA", (290, 386), "black")
+    img.paste(back, (727, 215))
 
 
 
 logo = Image.open("logo.png")
-img = Image.new("RGBA", (512, 384), None)
+img = Image.new("RGBA", (1024, 768), None)
 
 front = None
 if len(sys.argv) < 2:
