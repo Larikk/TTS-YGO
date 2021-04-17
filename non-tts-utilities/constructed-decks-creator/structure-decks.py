394d452dbde893972770534770693705cc579c71
larikk <larikk.dev@gmail.com>
2021-04-17 19:15:33 +0200

    Started with script to setup structure decks

---
+++ non-tts-utilities/constructed-decks-creator/structure-decks.py
@@ -0,0 +1,23 @@
+import lib.wiki as wiki
+
+def printDeck(deck):
+    print("Name", deck['name'])
+    print("Code", deck['code'])
+    print("Image", deck['image'])
+    print("Next", deck['next'])
+
+    cards = ""
+    for card in deck['cards']:
+        cards += card['Card number'] + " "
+        cards += card['Name'].ljust(60) + " "
+        cards += card['Rarity'].ljust(15) + " "
+        cards += card['Category'].ljust(30) + " "
+        if 'Qty' in card:
+            cards += card['Qty'] + " "
+        cards += "\n"
+
+    print(cards)
+
+title = "Structure Deck: Dragon's Roar"
+deck = wiki.download(title)
+printDeck(deck)
