d249b78a93fdfc09e8617d2886efdadd88e5a3fd
larikk <larikk.dev@gmail.com>
2021-04-18 15:53:51 +0200

    Added file writing

--- non-tts-utilities/constructed-decks-creator/structure-decks.py
+++ non-tts-utilities/constructed-decks-creator/structure-decks.py
@@ -1,13 +1,25 @@
 import lib.wiki as wiki
 import lib.imgur as imgur
 import lib.deckutil as deckutil
+import lib.files as files
+
+title = "Structure Deck: Dragon's Roar"
+counter = 1
+folder = "../../src/preconstructed-decks/structure-decks"
 
 albumHash = "dhSibop" # can be found in url
 imgur = imgur.Client(albumHash)
 
-title = "Structure Deck: Dragon's Roar"
 deck = wiki.download(title)
 deck['imgur'] = imgur.getUrl(deck['name'])
 deck['cards'] = deckutil.sortCards(deck)
 deck['ydk'] = deckutil.asYdkFile(deck)
 deckutil.printDeck(deck)
+
+# Write deck
+content = deckutil.asTtsLuaFile(deck)
+filename = f"{counter:03d} - {deck['name']}.ttslua"
+files.write(folder, filename, content)
+
+# Update decklist
+files.compileDeckList(folder, "src/preconstructed-decks/structure-decks")
