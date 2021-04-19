9485205ffe076c6b313d36da0c66bbdf5c51003d
larikk <larikk.dev@gmail.com>
2021-04-19 02:05:16 +0200

    updated starter deck script

--- non-tts-utilities/constructed-decks-creator/starter-decks.py
+++ non-tts-utilities/constructed-decks-creator/starter-decks.py
@@ -4,6 +4,7 @@ import lib.deckutil as deckutil
 import lib.files as files
 import os
 import atexit
+import time
 
 title = "Starter Deck: Yugi"
 counter = 1
@@ -34,8 +35,9 @@ nextReleaseExceptions = {
     "Starter Deck: Kaiba Evolution": "Starter Deck 2006",
     "Starter Deck 2006": "Starter Deck: Jaden Yuki",
     "Starter Deck: Xyz Symphony": "Super Starter: V for Victory",
-    "Starter Deck Kaiba Reloaded": "Super Starter: Space-Time Showdown",
+    "Starter Deck: Kaiba Reloaded": "Super Starter: Space-Time Showdown",
     "Super Starter: Space-Time Showdown": "Saber Force Starter Deck",
+    "Saber Force Starter Deck": "Dark Legion Starter Deck",
 }
 
 while True:
@@ -46,7 +48,9 @@ while True:
 
     if deck['name'] in nextReleaseExceptions:
         title = nextReleaseExceptions[deck['name']]
+    elif not deck['next'].startswith("Starter Deck"):
+        title = "Starter Deck: " + deck['next']
     else:
-    title = "Starter Deck: " + deck['next']
+        title = deck['next']
             
     counter += 1
