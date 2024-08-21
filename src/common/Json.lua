44d5b152de74395fb8bc02864e33523efb01826b
larikk <larikk.dev@gmail.com>
2024-08-21 21:44:35 +0200

    add temporary workaround for monsters with ? as atk/def

--- src/common/Json.lua
+++ src/common/Json.lua
@@ -1,5 +1,13 @@
 local module = {}
 
+
+local function temporaryHackyWorkaroundForNegativeNumbers(str)
+    str = string.gsub(str, [["atk":%-1]], [["atk":0]])
+    str = string.gsub(str, [["def":%-1]], [["def":0]])
+    return str
+end
+
+
 function module.convertUnicodeSequences(str)
     local pattern = [[\u[0-9a-fA-F]+]]
     local f = function(s) return string.sub(s, 1, 2) .. "{" .. string.sub(s, 3, 6) .. "}" .. string.sub(s, 7) end
@@ -8,6 +16,7 @@ end
 
 function module.decode(str)
     str = module.convertUnicodeSequences(str)
+    str = temporaryHackyWorkaroundForNegativeNumbers(str)
     return json.parse(str)
 end
 
