cd8b31bbd613cd8b3c08b71b44d1ded0cf609ec0
cellproductions <finalalert@hotmail.com>
2025-07-19 13:25:06 +1000

    Fixed sets including multiple copies of the same card (same rarity and set code) in their card pools.
    Some cards in sets (e.g. Cocoon of Evolution from Metal Raiders) can have both Short Print and Super Short Print rarity replacements (e.g. Common) and so one copy should be ignored from being included in the set's card pool.

--- src/pack-spawning/common/SetDataExtractor.lua
+++ src/pack-spawning/common/SetDataExtractor.lua
@@ -18,18 +18,33 @@ function SetDataExtractor:extract(cardApiData, name, codePrefixes)
         codePrefixes = { codePrefixes }
     end
 
-    local entries = {}
+    local entriesByRarity = {}
 
     for _, set in ipairs(cardApiData.card_sets) do
         if set.set_name == name and isSetCodeMatching(set.set_code, codePrefixes) then
-            local rarity = self.replacements[set.set_rarity] or set.set_rarity -- replace if necessary
-            if not self.ignoreList[rarity] then                                -- only add if the rarity is not ignored
+            local replacementRarity = self.replacements[set.set_rarity]
+            local rarity = nil
+            if replacementRarity then
+                -- multiple set entries can exist with the same replaced rarity (e.g. Short Print and Super Short Print)
+                -- avoid adding multiple entries with the same replaced rarity
+                if not entriesByRarity[replacementRarity] then
+                    rarity = replacementRarity
+                end
+            else
+                rarity = set.set_rarity
+            end
+            if rarity and not self.ignoreList[rarity] then -- only add if the rarity is not ignored
                 local entry = {}
                 entry.rarity = rarity
                 entry.setCode = set.set_code
-                table.insert(entries, entry)
+                entriesByRarity[entry.rarity] = entry
             end
         end
+    end
+
+    local entries = {}
+    for _, entry in pairs(entriesByRarity) do
+        table.insert(entries, entry)
     end
 
     for i, v in pairs(self.postListeners) do entries = v(entries) end
